528c09979f4fc42f2f8eb6a28d4b0e29b978bd89
OpasniyShkrek <cerubimkanal@gmail.com>
2023-08-05 13:44:54 +0300

    second commit

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
-# Create your models here.
+
+class Advertisements(models.Model):
+    title=models.CharField(
+        verbose_name='Заголовок',
+        help_text='Сюда пишем заголовок товара',
+        max_length=100,
+    )
+    description=models.TextField(
+        verbose_name='Описание'
+    )
+    price=models.DecimalField(
+        verbose_name='Цена',
+        max_digits=10,
+        decimal_places=2
+    )
+    auction=models.BooleanField(
+        verbose_name='Торг',
+        default=False
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date()==timezone.now().date():
+            created_time=self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;"> Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+
+
+    def __str__(self):
+        return f"id={self.id} title={self.title} price={self.price:.2f})"
+    class Meta:
+        db_table='Объявления'
+        verbose_name='Товар'
+        verbose_name_plural='Товары'
--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import index, top_sellers
 urlpatterns=[
-    path('', index),
-    path('top-sellers', top_sellers)
+    path('', index, name='main'),
+    path('top-sellers', top_sellers, name='top-sellers')
 ]
 
